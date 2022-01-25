1c4cf873f5ac776cd2762029d9802b1886d9b560
kholikulovz <kholikulov99@mail.ru>
2022-01-25 22:17:51 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.scss';
+// ------------------------
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer'
+
+function App() {
+  return (
+    <>
+      <Header/>
+    <div className='App'>
+{/* <main className="main"></main> */}
+    </div>
+      <Footer/>
+   </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header.scss'
+
+function Header() {
+  return( 
+  <>
+<header className='header'>
+  <div className='header__nav container'>
+  <ul className="header__left header__nav">
+    <li className="header__left-item header__nav-item">Pressa</li>
+    <li className="header__left-item header__nav-item">Kategoriya</li>
+  </ul>
+  <ul className="header__right header__nav">
+    <li className="header__right-item header__nav-item">Loyiha haqida</li>
+    <li className="header__right-item header__nav-item">Yordam</li>
+    <li className="header__right-item header__nav-item">E'lon berish</li>
+  </ul>
+  </div>
+</header>
+  </>
+  )
+}
+
+export default Header;
