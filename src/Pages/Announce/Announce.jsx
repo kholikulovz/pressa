5e1d80151f566ce78837c435d98e5e2efde2dcd1
kholikulovz <kholikulov99@mail.ru>
2022-02-05 00:47:18 +0500

    img-uploader

--- src/Pages/Announce/Announce.jsx
+++ src/Pages/Announce/Announce.jsx
@@ -1,16 +1,12 @@
 import React, { useRef, useState } from "react";
 import "./Announce.scss";
 import { Link } from "react-router-dom";
-// import { DatePicker } from "rsuite";
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import TimePicker from '@mui/lab/TimePicker';
 import DateTimePicker from '@mui/lab/DateTimePicker';
-import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
-import MobileDatePicker from '@mui/lab/MobileDatePicker';
-
+import Download from '../../Assets/Images/download.svg'
 
 function Announce() {
   //states
@@ -31,24 +27,6 @@ function Announce() {
   const desc = useRef();
 
   const [value, setValue] = useState(new Date('2022-01-01T00:00:00'));
-// const [state, setState] = useState({
-//     profileImg:
-//       "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-//   })
-//   state = {
-//     profileImg:
-//       "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-//   };
-//  const imageHandler = e => {
-//     const reader = new FileReader();
-//     reader.onload = () => {
-//       if (reader.readyState === 2) {
-//         this.setState({ profileImg: reader.result });
-//       }
-//     };
-//     reader.readAsDataURL(e.target.files[0]);
-//   };
-//   const { profileImg } = this.state;
 
   const handleChange = (newValue) => {
     setValue(newValue);
@@ -85,30 +63,10 @@ function Announce() {
           <form action="" className="announce__form">
             <ul className="announce__form-menu">
               <li className="announce__form-box announce__form-box--small">
-                {/* <label className="announce__form-label" htmlFor="">O’tkaziladigan sanani kiriting</label> */}
+                <label className="announce__form-label" htmlFor="">O’tkaziladigan sanani kiriting</label>
                 {/* <input className="announce__form-input" type="datetime-local" /> */}
                 <LocalizationProvider dateAdapter={AdapterDateFns}>
       <Stack spacing={3}>
-        {/* <DesktopDatePicker
-          label="Date desktop"
-          inputFormat="MM/dd/yyyy"
-          value={value}
-          onChange={handleChange}
-          renderInput={(params) => <TextField {...params} />}
-        /> */}
-        {/* <MobileDatePicker
-          label="Date mobile"
-          inputFormat="MM/dd/yyyy"
-          value={value}
-          onChange={handleChange}
-          renderInput={(params) => <TextField {...params} />}
-        />
-        <TimePicker
-          label="Time"
-          value={value}
-          onChange={handleChange}
-          renderInput={(params) => <TextField {...params} />}
-        /> */}
         <DateTimePicker className="announce__form-input"
           // label="Date&Time picker"
           value={value}
@@ -181,7 +139,8 @@ function Announce() {
         </div>
         <div className="announce__post announce__cards">
           <h3 className="announce__cards-title">Post</h3>
-          <form action="" className="announce__form">
+          <form action="" className="announce__form announce__form-sides">
+            <div className="announce__form--left">
             <ul className="announce__form-menu">
               <li className="announce__form-box announce__form-box--big">
                 <label className="announce__form-label" htmlFor="">Mavzu nomini kiriting</label>
@@ -200,28 +159,20 @@ function Announce() {
               <button className="announce__post-btn announce__post-btn--white" id="reject-btn">Bekor qilish</button>
               <button className="announce__post-btn announce__post-btn--blue" id="send-btn">Yuborish</button>
             </div>
-<input type="file" />
-{/* <div className="page">
-        <div className="my-container">
-          <h1 className="heading">Add your Image</h1>
-          <div className="img-holder">
-            <img src={profileImg} alt="" id="img" className="img" />
-          </div>
-          <input
-            type="file"
-            accept="image/*"
-            name="image-upload"
-            id="input"
-            onChange={this.imageHandler}
-          />
-          <div className="label">
-            <label className="image-upload" htmlFor="input">
-              <i className="material-icons">add_photo_alternate</i>
-              Choose your Photo
-            </label>
-          </div>
-        </div>
-      </div> */}
+            </div>
+            <div className="picture">
+            <div className="picture__box">
+        <input type="file" id="fileupload"  className="pic"/>
+          <span className="picture__span">
+            <img className="picture__img" src={Download} alt="" />
+            Mavzu rasmini yuklang</span>
+            </div>
+            <div className="picture__bottom">
+            <p className="picture__text">620x480 px o’lchamdagi rasm yuklash tavsiya etiladi.</p>
+            <div className="picture__preview" id="dvPreview"></div>
+            </div>
+            
+            </div>
           </form>
         </div>
       </div>
